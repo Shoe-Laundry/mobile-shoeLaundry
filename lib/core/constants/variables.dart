5cca1a7b67d35810df0ec66b307da57ad64f693e
M. Nizam Setiawan <nizamsetiawan15@gmail.com>
2024-12-07 02:46:00 +0700

    feat: add core components for project structure

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,5 @@
+class Variables {
+  static const String baseUrl = 'https://46c2-103-242-124-13.ngrok-free.app';
+  static const String imageBaseUrl = '$baseUrl/storage/products/';
+  static const String imageBaseUrl2 = '$baseUrl/storage/images/';
+}
